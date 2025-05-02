1deedf2da389e6719fc381f105ff85a4d7563158
Alexandr Shapkin <shapkin.alexandr.sergeevich@gmail.com>
2025-05-02 12:23:59 +0700

    feat:main.go refactoring

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log/slog"
+	"os"
 
 	"github.com/AlexandrShapkin/auth-go-test-task/app"
 	"github.com/AlexandrShapkin/auth-go-test-task/pkg/config"
@@ -10,13 +11,41 @@ import (
 	"github.com/AlexandrShapkin/auth-go-test-task/pkg/jwt"
 	"github.com/AlexandrShapkin/auth-go-test-task/pkg/repositories"
 	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func main() {
+// Функция обязана загрузить конфигурацию, иначе продолжать работу программы нет смысла
+func mustLoadConfig() *config.Config {
 	cfg, err := config.LoadConfig("config", "yaml", ".")
 	if err != nil {
 		slog.Error("Failed to load config", "error", err)
+		os.Exit(1) // насколько знаю в таких случаях используется panic(), для того чтобы ее можно было отловить, но не думаю что здесь это требуется
 	}
+	return cfg
+}
+
+// Функция обязана совершить успешное подключение к базе данных, иначе продолжать работу программы нет смысла
+func mustConnectDB(dbCfg config.Database) *gorm.DB {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=%s",
+		dbCfg.Host,
+		dbCfg.User,
+		dbCfg.Password,
+		dbCfg.DBName,
+		dbCfg.Port,
+		dbCfg.SSLMode,
+	)
+	dialector := postgres.Open(dsn)
+	database, err := db.ConnectDB(&dialector)
+	if err != nil {
+		slog.Error("Failed to connet database", "error", err)
+		os.Exit(1) // аналогично с комментарием оставленным в mustLoadConfig
+	}
+	return database
+}
+
+func main() {
+	cfg := mustLoadConfig()
 
 	jwtManager := jwt.NewJWT(
 		[]byte(cfg.JWT.AccessSecretKey),
@@ -26,20 +55,7 @@ func main() {
 		jwt.ParseLeewayWindow,
 	)
 
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%d sslmode=%s",
-		cfg.Database.Host,
-		cfg.Database.User,
-		cfg.Database.Password,
-		cfg.Database.DBName,
-		cfg.Database.Port,
-		cfg.Database.SSLMode,
-	)
-	dialector := postgres.Open(dsn)
-	database, err := db.ConnectDB(&dialector)
-	if err != nil {
-		panic(err)
-	}
+	database := mustConnectDB(cfg.Database)
 	userRepo := repositories.NewUserRepo(database)
 
 	application := app.NewApp(jwtManager, userRepo, cfg.App.LoginRemoteIPMode, cfg.App.RefreshRemoteIPMode)
